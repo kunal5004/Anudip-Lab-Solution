91b25e3039b07dcd72f6283427b5208314dd6dd0
kunal5004 <Masterkunald@gmail.com>
2024-09-12 11:58:48 +0530

    Add files via upload

---
+++ Python_Day14_Tuples_Assignment1.py
@@ -0,0 +1,11 @@
+#Write a Python program to find the number of times 4 appears in the tuple.
+
+#Create a tuple.
+tuplex = (2, 4, 5, 6, 2, 3, 4, 4, 7 )
+#Use count to find how many time 4 appeared in tuple.
+result= tuplex.count(4)
+#print the result.
+print(f"Number of times 4 appears: {result}")
+
+#Output
+#Number of times 4 appears: 3
---
+++ Python_Day14_Tuples_Assignment2.py
@@ -0,0 +1,11 @@
+#Write a Python program to convert a list to a tuple.
+
+#Create a List
+listx = [5, 10, 7, 4, 15, 3]
+#Convert the list into a tuple using the tuple() function
+my_tuple=tuple(listx)
+#Display the tuple
+print(my_tuple)
+
+#Output
+#(5, 10, 7, 4, 15, 3)
